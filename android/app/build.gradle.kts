7e3d20e8f15842aa7c12874c79d6aa94a6d40ede
vizenturelabs <260604164+vizenturelabs@users.noreply.github.com>
2026-09-25 10:40:07 +0100

    Update commit for CypRide

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -46,8 +46,8 @@ android {
         applicationId = "com.vizenture.cypride"
         minSdk = flutter.minSdkVersion
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.1.3"
+        versionCode = 10
+        versionName = "1.1.4"
 
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a", "x86_64")
